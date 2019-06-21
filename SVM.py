1c4b03cb3180a9f6f3e0f5cccd01b4ad20f88869
Owner <owner@MacbookPro-nekketsu.local>
2019-06-21 17:07:41 +0900

    model2 create SVM.py

--- SVM.py
+++ SVM.py
@@ -9,16 +9,22 @@ import decimal
 
 fileNames = ['train_hips.npy', 'train_torso.npy']
 
-data = np.loadtxt('data_0615/train_bag.npy', delimiter=',', skiprows=1)
+data = np.loadtxt('train_bag.npy', delimiter=',', skiprows=1)
 label = data[:, 0]
-feautures = data[:, 1:]
+# features xy_accel_mean variance
+# z_accel_mean variance
+# xyz_gra_mean variance
+# xyz_gyr_mean ariance
+# xyz_mag_mean variance
+# pressure_difference
+feautures = data[:, [1, 2, 3, 4, 9, 10]] #加速度と地磁気だけを取り出した
 X = feautures
 Y = label
 
 for fileName in fileNames:
-    data = np.loadtxt("data_0615/" + fileName, delimiter=',', skiprows=1)
+    data = np.loadtxt(fileName, delimiter=',', skiprows=1)
     label = data[:, 0]
-    feautures = data[:, 1:]
+    feautures = data[:, [1, 2, 3, 4, 9, 10]]  # 加速度と地磁気だけを取り出した
     X = np.vstack((X, feautures))
     Y = np.hstack((Y, label))
 
@@ -27,9 +33,9 @@ for fileName in fileNames:
 Y = Y.reshape((Y.shape[0], ))
 
 #標準化のみ（画像のRGBに正規化はいいらしいので）
-# stdsc = StandardScaler()
-# X_std = stdsc.fit_transform(X)
-# print(X_std[0:5])
+stdsc = StandardScaler()
+X_std = stdsc.fit_transform(X)
+print(X_std[0:5])
 with open('data_0615/stdFile.binaryfile', 'wb') as file:
     pickle.dump(stdsc, file)
 
@@ -37,15 +43,8 @@ with open('data_0615/stdFile.binaryfile', 'wb') as file:
 # X_std[np.isnan(X_std)] = 0
 X_std[np.isnan(X_std)] = -1
 
-with open('model_next.binaryfile', 'rb') as file:
-    clf1 = pickle.load(file)
-scoring = "f1_macro"
-scores = cross_val_score(clf1, X_std, Y, cv=10, scoring=scoring)
-print("{}:{:.3f}+/-{:.3f}".format(scoring, scores.mean(), scores.std()))
-exit()
-
 #学習開始！
-clf1 = svm.SVC(verbose=True)
-clf1.fit(X_std, Y)
+clf2 = svm.SVC(verbose=True)
+clf2.fit(X_std, Y)
 with open('model_next.binaryfile', 'wb') as file:
-    pickle.dump(clf1, file)
+    pickle.dump(clf2, file)
